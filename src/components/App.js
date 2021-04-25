c9f65536fa13fd22c5a99db73987f447420c4b48
Daniel <danisolo91@gmail.com>
2021-04-25 23:13:34 +0200

    Add some routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,54 +1,24 @@
-import { useEffect, useState } from 'react';
-import { database } from '../firebase';
-import { makeStyles } from '@material-ui/core/styles';
-import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-
-const useStyles = makeStyles({
-    root: {
-        maxWidth: 275,
-    },
-});
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
+import Home from "./Home";
+import SignIn from './SignIn';
+import SignUp from './SignUp';
 
 const App = () => {
-
-    const classes = useStyles();
-
-    const [person, setPerson] = useState(null);
-
-    useEffect(() => {
-        database.example.get().then(results => {
-            setPerson(database.formatDoc(results.docs[0]));
-        })
-    }, []);
-
     return (
         <>
-            Hello {person ? person.name : <>...</>}!
-            <FavoriteBorderIcon fontSize="small" />
-            <br/><br/>
-            <Card className={classes.root}>
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
-                        Lizard
-                    </Typography>
-                    Hello Daniel!<br/>
-                </CardContent>
-                <CardActions>
-                    <Button 
-                        size="small" 
-                        variant="contained" 
-                        color="primary"
-                        disableElevation
-                    >Learn More</Button>
-                </CardActions>
-            </Card>
+            <Router>
+                <Switch>
+                    <Route exact path="/sign-in" component={SignIn} />
+                    <Route path="/sign-up" component={SignUp} />
+                    <Route path="/home" component={Home} />
+                </Switch>
+            </Router>
         </>
     );
-};
+}
 
 export default App;
