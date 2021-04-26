29944315ddfbd8dd83d3b7155c519ac8128a1b47
Daniel <danisolo91@gmail.com>
2021-04-26 22:41:42 +0200

    Implement SignUp functionality

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link as RouterLink } from 'react-router-dom';
+import { Link as RouterLink, useHistory } from 'react-router-dom';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
@@ -12,20 +12,10 @@ import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-
-function Copyright() {
-    return (
-        <Typography variant="body2" color="textSecondary" align="center">
-            {'Copyright © '}
-            <Link color="inherit" href="https://material-ui.com/">
-                Twitty
-            </Link>
-            {' '}
-            {new Date().getFullYear()}
-            {'.'}
-        </Typography>
-    );
-}
+import Alert from '@material-ui/lab/Alert';
+import Copyright from './Copyright';
+import { useState } from 'react';
+import useAuth from '../hooks/useAuth';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -56,10 +46,49 @@ const useStyles = makeStyles((theme) => ({
     submit: {
         margin: theme.spacing(3, 0, 2),
     },
+    alert: {
+        marginBottom: theme.spacing(2),
+    },
 }));
 
-export default function SignUp() {
+const SignUp = () => {
     const classes = useStyles();
+    const history = useHistory();
+    const { signUp } = useAuth();
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [input, setInput] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        passwordConfirm: '',
+    });
+
+    const inputHandler = (e) => {
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value,
+        });
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        
+        if(input.password !== input.passwordConfirm) {
+            return setError('Passwords do not match');
+        }
+
+        try {
+            setError(null);
+            setLoading(true);
+            await signUp(input.email, input.password);
+            history.push('/');
+        } catch {
+            setError('Failed to create an account');
+            setLoading(false);
+        }
+    }
 
     return (
         <Grid container component="main" className={classes.root}>
@@ -72,70 +101,97 @@ export default function SignUp() {
                     <Typography component="h1" variant="h5">
                         Sign up
                     </Typography>
-                    <form className={classes.form} noValidate>
-                        <Grid container spacing={2}>
-                            <Grid item xs={12} sm={6}>
-                                <TextField
-                                    autoComplete="fname"
-                                    name="firstName"
-                                    variant="outlined"
-                                    required
-                                    fullWidth
-                                    id="firstName"
-                                    label="First Name"
-                                    autoFocus
-                                />
+                    <form className={classes.form} onSubmit={handleSubmit}>
+                        {error && <Alert severity="error" className={classes.alert}>{error}</Alert>}
+                        <fieldset disabled={loading}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={12} sm={6}>
+                                    <TextField
+                                        autoComplete="fname"
+                                        name="firstName"
+                                        variant="outlined"
+                                        required
+                                        fullWidth
+                                        id="firstName"
+                                        label="First Name"
+                                        autoFocus
+                                        value={input.firstName}
+                                        onChange={inputHandler}
+                                    />
+                                </Grid>
+                                <Grid item xs={12} sm={6}>
+                                    <TextField
+                                        variant="outlined"
+                                        required
+                                        fullWidth
+                                        id="lastName"
+                                        label="Last Name"
+                                        name="lastName"
+                                        autoComplete="lname"
+                                        value={input.lastName}
+                                        onChange={inputHandler}
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        variant="outlined"
+                                        required
+                                        fullWidth
+                                        id="email"
+                                        label="Email Address"
+                                        name="email"
+                                        autoComplete="email"
+                                        value={input.email}
+                                        onChange={inputHandler}
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        variant="outlined"
+                                        required
+                                        fullWidth
+                                        name="password"
+                                        label="Password"
+                                        type="password"
+                                        id="password"
+                                        autoComplete="current-password"
+                                        value={input.password}
+                                        onChange={inputHandler}
+                                        inputProps={{ minLength: 6 }}
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        variant="outlined"
+                                        required
+                                        fullWidth
+                                        name="passwordConfirm"
+                                        label="Password confirm"
+                                        type="password"
+                                        id="passwordConfirm"
+                                        autoComplete="current-password"
+                                        value={input.passwordConfirm}
+                                        onChange={inputHandler}
+                                        inputProps={{ minLength: 6 }}
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <FormControlLabel
+                                        control={<Checkbox value="allowExtraEmails" color="primary" />}
+                                        label="I agree with the Terms of Service and Privacy Policy."
+                                    />
+                                </Grid>
                             </Grid>
-                            <Grid item xs={12} sm={6}>
-                                <TextField
-                                    variant="outlined"
-                                    required
-                                    fullWidth
-                                    id="lastName"
-                                    label="Last Name"
-                                    name="lastName"
-                                    autoComplete="lname"
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    variant="outlined"
-                                    required
-                                    fullWidth
-                                    id="email"
-                                    label="Email Address"
-                                    name="email"
-                                    autoComplete="email"
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    variant="outlined"
-                                    required
-                                    fullWidth
-                                    name="password"
-                                    label="Password"
-                                    type="password"
-                                    id="password"
-                                    autoComplete="current-password"
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <FormControlLabel
-                                    control={<Checkbox value="allowExtraEmails" color="primary" />}
-                                    label="I want to receive inspiration, marketing promotions and updates via email."
-                                />
-                            </Grid>
-                        </Grid>
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            color="primary"
-                            className={classes.submit}
-                        >
-                            Sign Up
-                        </Button>
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                color="primary"
+                                className={classes.submit}
+                            >
+                                Sign Up
+                            </Button>
+                        </fieldset>
                         <Grid container justify="flex-end">
                             <Grid item>
                                 <Link component={RouterLink} to="/sign-in" variant="body2">
@@ -152,3 +208,5 @@ export default function SignUp() {
         </Grid>
     );
 }
+
+export default SignUp;
