18e1c17ef18fa7d374ce946298b30188dd664162
Daniel <danisolo91@gmail.com>
2021-04-26 22:39:30 +0200

    Add private routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,24 @@
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import { AuthProvider } from "../contexts/AuthContext";
 import Home from "./Home";
+import PrivateRoute from "./PrivateRoute";
 import SignIn from './SignIn';
 import SignUp from './SignUp';
 
 const App = () => {
     return (
-        <>
-            <Router>
+        <Router>
+            <AuthProvider>
                 <Switch>
-                    <Route exact path="/sign-in" component={SignIn} />
+                    <Route exact path="/">
+                        <Redirect to="/home" />
+                    </Route>
+                    <PrivateRoute path="/home" component={Home} />
+                    <Route path="/sign-in" component={SignIn} />
                     <Route path="/sign-up" component={SignUp} />
-                    <Route path="/home" component={Home} />
                 </Switch>
-            </Router>
-        </>
+            </AuthProvider>
+        </Router>
     );
 }
 
