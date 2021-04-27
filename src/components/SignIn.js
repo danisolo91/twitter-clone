c46e971b0e3613dbeace73399a6f07cfa5da9827
Daniel <danisolo91@gmail.com>
2021-04-27 23:02:10 +0200

    Implement SignIn

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link as RouterLink } from 'react-router-dom';
+import { useState } from 'react';
+import { Link as RouterLink, useHistory } from 'react-router-dom';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
@@ -12,7 +12,9 @@ import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import Alert from '@material-ui/lab/Alert';
 import Copyright from './Copyright';
+import useAuth from '../hooks/useAuth';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -43,11 +45,42 @@ const useStyles = makeStyles((theme) => ({
     submit: {
         margin: theme.spacing(3, 0, 2),
     },
+    alert: {
+        marginBottom: theme.spacing(2),
+    },
 }));
 
 const SignIn = () => {
     const classes = useStyles();
+    const { signIn } = useAuth();
+    const history = useHistory();
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [input, setInput] = useState({
+        email: '',
+        password: ''
+    });
 
+    const handleInput = (e) => {
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value,
+        });
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            setError(null);
+            setLoading(true);
+            await signIn(input.email, input.password);
+            history.push('/');
+        } catch {
+            setError('Failed to sign in');
+            setLoading(false);
+        }
+    }
+ 
     return (
         <Grid container component="main" className={classes.root}>
             <Grid item xs={false} sm={4} md={7} className={classes.image} />
@@ -59,42 +92,46 @@ const SignIn = () => {
                     <Typography component="h1" variant="h5">
                         Sign in
                     </Typography>
-                    <form className={classes.form}>
-                        <TextField
-                            variant="outlined"
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="email"
-                            label="Email Address"
-                            name="email"
-                            autoComplete="email"
-                            autoFocus
-                        />
-                        <TextField
-                            variant="outlined"
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Password"
-                            type="password"
-                            id="password"
-                            autoComplete="current-password"
-                        />
-                        <FormControlLabel
-                            control={<Checkbox value="remember" color="primary" />}
-                            label="Remember me"
-                        />
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            color="primary"
-                            className={classes.submit}
-                        >
-                            Sign In
-                        </Button>
+                    <form className={classes.form} onSubmit={handleSubmit}>
+                        {error && <Alert severity="error" className={classes.alert}>{error}</Alert>}
+                        <fieldset disabled={loading}>
+                            <TextField
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                                autoFocus
+                                onChange={handleInput}
+                            />
+                            <TextField
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                onChange={handleInput}
+                            />
+                            <FormControlLabel
+                                control={<Checkbox value="remember" color="primary" />}
+                                label="Remember me"
+                            />
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                color="primary"
+                                className={classes.submit}
+                            >
+                                Sign In
+                            </Button>
+                        </fieldset>
                         <Grid container>
                             <Grid item xs>
                                 <Link component={RouterLink} to="/home" variant="body2">
