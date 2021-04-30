40fb9e92f7d7fa1c21f05248a76197637bd45395
Daniel <danisolo91@gmail.com>
2021-04-30 22:59:37 +0200

    Create Profile component

---
+++ src/components/Profile.js
@@ -0,0 +1,31 @@
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        padding: theme.spacing(2),
+        display: 'flex',
+        overflow: 'auto',
+        flexDirection: 'column',
+    },
+    fixedHeight: {
+        height: 240,
+    },
+}));
+
+const Profile = () => {
+    const classes = useStyles();
+    const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+    
+    return (
+        <Grid item xs={12} md={12} lg={12}>
+            <Paper className={fixedHeightPaper}>
+                Profile
+            </Paper>
+        </Grid>
+    );
+};
+
+export default Profile;
